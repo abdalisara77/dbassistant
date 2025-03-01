100824aa27e335aa045990abf4c911630de03dc0
Sara Abdali <saraabd@stanford.edu>
2025-03-01 09:51:23 -0500

    adding direct entry point

---
+++ run_assistant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Entry point script to run the database assistant.
+"""
+
+from scripts.run_dbassistant import run_assistant
+
+if __name__ == "__main__":
+    run_assistant()
